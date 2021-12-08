fbcff2a0395905747ffb3a206f276d3dcefaa366
Renan Lopes <renanlopes.ads@gmail.com>
2021-12-08 19:40:13 -0300

    Config Layout Razor Page

--- AspNetCoreIdentity/Startup.cs
+++ AspNetCoreIdentity/Startup.cs
@@ -60,6 +60,8 @@ namespace AspNetCoreIdentity
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
