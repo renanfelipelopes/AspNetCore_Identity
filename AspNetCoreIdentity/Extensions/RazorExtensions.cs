67d5a924bdfca004fd4f56db83c27c87b500f5ac
Renan Lopes <renanlopes.ads@gmail.com>
2021-12-10 20:50:58 -0300

    RazorExtensions

---
+++ AspNetCoreIdentity/Extensions/RazorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentity.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static bool IfClaim(this RazorPage page, string claimName, string claimValue)
+        {
+            return CustomAuthorization.ValidarClaimsUsuario(page.Context, claimName, claimValue);
+        }
+
+        public static string IfClaimShow(this RazorPage page, string claimName, string claimValue)
+        {
+            return CustomAuthorization.ValidarClaimsUsuario(page.Context, claimName, claimValue) ? "" : "disabled";
+        }
+
+        public static IHtmlContent IfClaimShow(this IHtmlContent page, HttpContext context, string claimName, string claimValue)
+        {
+            return CustomAuthorization.ValidarClaimsUsuario(context, claimName, claimValue) ? page : null;
+        }
+    }
+}
